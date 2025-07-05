1897dd1f752ff52cfdf657257de2f1533c0e914a
TomKaro3 <144401994+TomKaro3@users.noreply.github.com>
2025-07-05 19:30:33 +1000

    sd

--- ipl-tips-backend/index.js
+++ ipl-tips-backend/index.js
@@ -7,10 +7,11 @@ const app = express();
 dotenv.config();
 app.use(cors({
   origin: [
-    "http://localhost:5173",               // local dev
+    "http://localhost:3000", 
+    "http://localhost:3001",               // local dev
     "https://ipl-tips.vercel.app"          // your Vercel frontend
   ],
-  credentials: true, // ⬅️ allow cookies/session
+  credentials: true, // ⬅️ allow  cookies/session
 }));
 app.use(express.json());
 
