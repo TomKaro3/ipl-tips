e2fcf891d3cb67c8a9e7c034f106e4c9f150175b
TomKaro3 <144401994+TomKaro3@users.noreply.github.com>
2025-07-05 12:43:38 +1000

    make work

--- ipl-tips-backend/index.js
+++ ipl-tips-backend/index.js
@@ -5,7 +5,13 @@ const dotenv = require('dotenv');
 const app = express();
 
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",               // local dev
+    "https://ipl-tips.vercel.app"          // your Vercel frontend
+  ],
+  credentials: true, // ⬅️ allow cookies/session
+}));
 app.use(express.json());
 
 // Routes
