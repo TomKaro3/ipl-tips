3e9b0854ee9b743a0d31b8b94021363345373fcd
TomKaro3 <144401994+TomKaro3@users.noreply.github.com>
2025-07-05 13:27:28 +1000

    g

--- ipl-tips-backend/index.js
+++ ipl-tips-backend/index.js
@@ -13,10 +13,6 @@ app.use(cors({
   credentials: true, // ⬅️ allow cookies/session
 }));
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`🔥 Incoming request: ${req.method} ${req.url}`);
-  next();
-});
 
 // Routes
 const matchRoutes = require('./routes/matches');
