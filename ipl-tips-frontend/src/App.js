86d20c22de851080d82097fc553565240e27f470
TomKaro3 <144401994+TomKaro3@users.noreply.github.com>
2025-07-10 21:09:03 +1000

    directs user to login if not logged in

--- ipl-tips-frontend/src/App.js
+++ ipl-tips-frontend/src/App.js
@@ -5,6 +5,7 @@ import TipPage from "./pages/TipPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
 import AdminPage from "./pages/AdminPage";
 import AdminOnlyRoute from "./components/AdminOnlyRoute";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -12,10 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/tips" element={<TipPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="*" element={<PrivateRoute><Navigate to="/" /></PrivateRoute>} />
+        <Route path="/tips" element={<PrivateRoute><TipPage /></PrivateRoute>} />
+        <Route path="/leaderboard" element={<PrivateRoute><LeaderboardPage /></PrivateRoute>} />
         <Route path="/admin" element={<AdminOnlyRoute><AdminPage /></AdminOnlyRoute>} />
       </Routes>
     </BrowserRouter>
